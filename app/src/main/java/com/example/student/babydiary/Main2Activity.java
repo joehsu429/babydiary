36bd4107fd76cb4bee9ab0e5f53d761aa516941c
joe <kkwhsu@gmail.com>
2018-01-17 16:55:34 +0800

    add main6

--- app/src/main/java/com/example/student/babydiary/Main2Activity.java
+++ app/src/main/java/com/example/student/babydiary/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.student.babydiary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+    }
+    public void click5(View v)
+    {
+        Intent it = new Intent(this,Main6Activity.class);
+        startActivity(it);
     }
 }
--- app/src/main/java/com/example/student/babydiary/MainActivity.java
+++ app/src/main/java/com/example/student/babydiary/MainActivity.java
@@ -1,27 +1,36 @@
 package com.example.student.babydiary;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
 
 
 public class MainActivity extends AppCompatActivity{
-    TextView tv,tv2,tv3;
+    TextView tv,tv2,tv3,tv4;
     EditText ed,ed2;
-
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = new TextView(this);
-        TextView tv2 = new TextView(this);
-        TextView tv3 = new TextView(this);
+        tv = new TextView(this);
+        tv2 = new TextView(this);
+        tv3 = new TextView(this);
+        tv4 = new TextView(this);
 
-
+    }
+    public void click1(View v)
+    {
+        Intent it = new Intent(this,Main2Activity.class);//跳第2頁
+        startActivity(it);
     }
 
 }
