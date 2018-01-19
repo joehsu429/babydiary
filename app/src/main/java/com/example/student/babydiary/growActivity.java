ddc76a37330f0120bb355b644beb92a53ebb9666
MarsChang <mars09250148@yahoo.com.tw>
2018-01-19 13:05:48 +0800

    增加成長紀錄activity

---
+++ app/src/main/java/com/example/student/babydiary/growActivity.java
@@ -0,0 +1,76 @@
+package com.example.student.babydiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class growActivity extends AppCompatActivity {
+    TextView tv6;
+    /*声明日期及时间变量*/
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    private int mHour;
+    private int mMinute;
+    /*声明对象变量*/
+    TimePicker tp;
+    DatePicker dp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grow);
+        /*取得目前日期与时间*/
+        Calendar c=Calendar.getInstance();
+        mYear=c.get(Calendar.YEAR);
+        mMonth=c.get(Calendar.MONTH);
+        mDay=c.get(Calendar.DAY_OF_MONTH);
+        mHour=c.get(Calendar.HOUR_OF_DAY);
+        mMinute=c.get(Calendar.MINUTE);
+        tv6 = (TextView) findViewById(R.id.textView6);
+        updateDisplay();
+        dp = new DatePicker(growActivity.this);
+        dp.init(mYear,mMonth,mDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int  year, int monthOfYear, int dayOfMonth) {
+                mYear=year;
+                mMonth= monthOfYear;
+                mDay=dayOfMonth;
+                /*调用updateDisplay()来改变显示日期*/
+                updateDisplay();
+            }
+        });
+        /*取得TimePicker对象，并设置为24小时制显示*/
+        tp = new TimePicker(growActivity.this);
+        tp.setIs24HourView(true);
+        /*setOnTimeChangedListener，并重写onTimeChanged event*/
+        tp.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hourOfDay, int minute) {
+                mHour=hourOfDay;
+                mMinute=minute;
+                /*调用updateDisplay()来改变显示时间*/
+                updateDisplay();
+            }
+        });
+    }
+
+    private void updateDisplay() {
+        tv6.setText(
+                new StringBuilder().append(mYear).append("/")
+                        .append(format(mMonth + 1)).append("/")
+                        .append(format(mDay)).append("　")
+                        .append(format(mHour)).append(":")
+                        .append(format(mMinute)));
+    }
+    /*日期时间显示两位数的方法*/
+    private String format(int x)
+    {
+        String s=""+x;
+        if(s.length()==1) s="0"+s;
+        return s;
+    }
+}
